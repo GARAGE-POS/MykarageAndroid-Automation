1dcfabb9e8aded4fd7a73ce103a170317ef9b21c
Eatasam <eatasams@gmail.com>
2024-11-06 16:20:57 +0300

    Second Commit Message

---
+++ project/update_reports.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+cd /c/Users/eatas/eclipse-workspace/Garage_CustomerApp_repo/MykarageAndroid-Automation/project
+  # Make sure this path is correct
+git add report/ screenshots/
+git commit -m "Update reports and screenshots"
+git push origin main
